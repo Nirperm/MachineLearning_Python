672220121be4ba62b97bb34eb444ea8a22eee369
nirperm <nirperm@gmail.com>
2015-06-24 06:18:24 +0000

    finish chapter3

--- NLP_100knock/chapter3/29.py
+++ NLP_100knock/chapter3/29.py
@@ -4,8 +4,9 @@
 （ヒント: MediaWiki APIのimageinfoを呼び出して，ファイル参照をURLに変換すればよい）
 """
 
+import json
 import re
-import urllib
+from urllib import request
 from section_20 import load_json
 from section_20 import extract_uk_context
 
@@ -33,6 +34,10 @@ for template_tuple in template_list:
     template_dict[template_tuple[0].strip()] = trim_value
 
 flag_image = template_dict['国旗画像']
-url = 'https://en.wikipedia.org/w/api.php'
-params = '?query&titles=Image:{0}&prop=imageinfo'.format(flag_image)
+flag_image = flag_image.replace(' ', '%20')  # TODO: is this correct?
+url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&titles=Image:{0}&prop=imageinfo&iiprop=url'.format(flag_image)
 
+req = request.urlopen(url)
+encoding = req.headers.get_content_charset()
+req_json = json.loads(req.read().decode(encoding))
+print(req_json['query']['pages']['23473560']['imageinfo'][0]['url'])
