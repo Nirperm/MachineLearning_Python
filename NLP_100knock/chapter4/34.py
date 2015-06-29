ab6d110f2c8ccd24960eb4967f88331222eebd17
nirperm <nirperm@gmail.com>
2015-06-29 00:20:27 +0000

    35(ongoing)

--- NLP_100knock/chapter4/34.py
+++ NLP_100knock/chapter4/34.py
@@ -13,8 +13,8 @@ for i, mecab_dict in enumerate(mecab_dict_list):
     if mecab_dict['pos1'] == '連体化' and \
         mecab_dict_list[i - 1]['pos'] == '名詞' and \
         mecab_dict_list[i + 1]['pos'] == '名詞':
-                nominal_list.append(mecab_dict_list[i - 1]['surface'] + \
-                                           mecab_dict['base'] + \
-                                           mecab_dict_list[i + 1]['surface'])
+            nominal_list.append(mecab_dict_list[i - 1]['surface'] + \
+                                mecab_dict['base'] + \
+                                mecab_dict_list[i + 1]['surface'])
 
 print(list(set(nominal_list)))
--- NLP_100knock/chapter4/35.py
+++ NLP_100knock/chapter4/35.py
@@ -8,3 +8,13 @@ from section_30 import make_mecab_data
 
 non_juncture_list = []
 mecab_dict_list = make_mecab_data()
+
+for i, mecab_dict in enumerate(mecab_dict_list):
+    if mecab_dict['pos'] == '名詞' and \
+        (mecab_dict_list[i - 1]['pos'] == '名詞' or \
+         mecab_dict_list[i + 1]['pos'] == '名詞'):
+            non_juncture_list.append(mecab_dict_list[i - 1]['surface'] + \
+                                     mecab_dict['base'] + \
+                                     mecab_dict_list[i + 1]['surface'])
+
+print(non_juncture_list)
