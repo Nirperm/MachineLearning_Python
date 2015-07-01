c764ecf29299f2da824983ccbbee001e432fe09d
nirperm <nirperm@gmail.com>
2015-07-01 00:22:07 +0000

    finish 35

--- NLP_100knock/chapter4/35.py
+++ NLP_100knock/chapter4/35.py
@@ -1,20 +1,22 @@
 """
 35. 名詞の連接
 名詞の連接（連続して出現する名詞）を最長一致で抽出せよ．
+
 """
 
 
 from section_30 import make_mecab_data
 
+# 最大、9-ngram
 non_juncture_list = []
 mecab_dict_list = make_mecab_data()
 
 for i, mecab_dict in enumerate(mecab_dict_list):
-    if mecab_dict['pos'] == '名詞' and \
-        (mecab_dict_list[i - 1]['pos'] == '名詞' or \
-         mecab_dict_list[i + 1]['pos'] == '名詞'):
-            non_juncture_list.append(mecab_dict_list[i - 1]['surface'] + \
-                                     mecab_dict['base'] + \
-                                     mecab_dict_list[i + 1]['surface'])
+    try:
+        if mecab_dict['pos'] == '名詞':
+            if mecab_dict_list[i + 1]['pos'] == '名詞':
+                non_juncture_list.append(mecab_dict['surface'] + mecab_dict_list[i + 1]['surface'])
+    except IndexError:
+        pass
 
 print(non_juncture_list)
