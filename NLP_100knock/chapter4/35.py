75303329fbdaf07b73ab5338d55613af1842c217
nirperm <nirperm@gmail.com>
2015-07-01 04:01:55 +0000

    tweak 35(ongoing)

--- NLP_100knock/chapter4/35.py
+++ NLP_100knock/chapter4/35.py
@@ -6,17 +6,29 @@
 
 
 from section_30 import make_mecab_data
+import re
 
 # 最大、9-ngram
-non_juncture_list = []
+non_list = []
 mecab_dict_list = make_mecab_data()
 
 for i, mecab_dict in enumerate(mecab_dict_list):
     try:
-        if mecab_dict['pos'] == '名詞':
-            if mecab_dict_list[i + 1]['pos'] == '名詞':
-                non_juncture_list.append(mecab_dict['surface'] + mecab_dict_list[i + 1]['surface'])
+        # TODO 連続していない名詞もとれている
+        if mecab_dict['pos'] == '名詞' and mecab_dict_list[i]['pos'] == '名詞':
+            non_list.append(mecab_dict['surface'])
+        else:
+            non_list.append('@')
+
     except IndexError:
         pass
+
+str_non = '@'.join(non_list)
+result = re.sub(r'@{2,}', ',', str_non)
+replace_list = result.replace('@', '').split(',')
+non_juncture_list = []
+for word in replace_list:
+    if len(word) >= 2:
+        non_juncture_list.append(word)
 
 print(non_juncture_list)
