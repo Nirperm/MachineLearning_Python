9d868c2e544c1f412755be1f73f414483acf797d
nirperm <nirperm@gmail.com>
2015-07-23 07:03:43 +0000

    implementation

---
+++ NLP_100knock/chapter8/72.py
@@ -0,0 +1,46 @@
+"""
+72. 素性抽出
+極性分析に有用そうな素性を各自で設計し，学習データから素性を抽出せよ．
+素性としては，レビューからストップワードを除去し，
+各単語をステミング処理したものが最低限のベースラインとなるであろう．
+"""
+
+import re
+from constant import STOP_WORD_LIST
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+
+
+def extract_sentence(content_list):
+    sentence_list = []
+    for word in content_list:
+        if word not in STOP_WORD_LIST:
+            sentence_list.append(word)
+    return sentence_list
+
+def stemmer(sentence_list):
+    stemmer = PorterStemmer()
+    lemmatiser = WordNetLemmatizer()
+
+    for word in sentence_list:
+        word = re.sub(r"^'", '', word)
+        word = re.sub(r"^-", '', word)
+        word = re.sub(r"'$", '', word)
+        word = re.sub(r"^\d*-\S+", '', word)
+        word = re.sub(r"^\d+\/\d", '', word)
+        word = re.sub(r"^\d+", '', word)
+        if len(word) is not 0:
+            print(lemmatiser.lemmatize(word, pos='v'))
+            # print(stemmer.stem(word.strip()))
+
+
+if __name__ == '__main__':
+    with open('data/sentiment.txt', encoding='utf-8') as f:
+        content = f.read()
+    content_list = content.replace('\t', ' ') \
+                          .replace('</>', '') \
+                          .replace('[', '') \
+                          .replace(']', '') \
+                          .strip().split()
+
+    sentence_list = extract_sentence(content_list)
+    stemmer(sentence_list)
