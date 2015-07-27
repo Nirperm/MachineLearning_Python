ddf1e506799327f1aaacc67a722e233ee6ce792b
nirperm <nirperm@gmail.com>
2015-07-27 06:09:51 +0000

    insert data to mongodb(pending)

--- NLP_100knock/chapter7/64.py
+++ NLP_100knock/chapter7/64.py
@@ -13,15 +13,20 @@ db = client.artist
 col = db.artists
 
 f = gzip.open('data/artist.json.gz', 'rt')
+
+
 for line in f:
     data = json.loads(line)
+    col.insert({'artist_info': data})
+    """
     if 'aliases' in data:
         for i in range(len(data['aliases'])):
-            data['aliases'][i]['name']
+            col.ensureIndex({'data': data, 'aliase_id': i, 'aliases_name': data['aliases'][i]['name']})
     if 'tags' in data:
         for i in range(len(data['tags'])):
-            data['tags'][i]['value']
+            col.ensureIndex({'data': data, 'tag_id': i, 'tags': data['tags'][i]['value']})
     if 'rating' in data:
-        data['rating']['value']
-
-    col.insert({'name': data['name'], 'aliases.name': '', 'tags.value': '', 'rating.value': '')
+        for i in range(len(data['rating'])):
+            col.ensureIndex({'data': data, 'rating_id': i, 'rating_value': data['rating']['value']})
+    """
+f.close()
