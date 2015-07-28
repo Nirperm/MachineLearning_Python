73ff17dc10259b1ea46fd19873a6940602a7a7c2
nirperm <nirperm@gmail.com>
2015-07-28 03:40:44 +0000

    finish? 72

--- NLP_100knock/chapter8/72.py
+++ NLP_100knock/chapter8/72.py
@@ -7,9 +7,10 @@
 
 import re
 from constant import STOPWORDS
+from gensim import corpora
+# from gensim import models
 from nltk.stem import WordNetLemmatizer
 
-# TODO: create data index colum neg/pos word
 
 def extract_sentence(content_list):
     sentence_list = []
@@ -21,7 +22,6 @@ def extract_sentence(content_list):
 
 def stemmer(sentence_list):
     lemmatiser = WordNetLemmatizer()
-
     for word in sentence_list:
         word = re.sub(r"^'", '', word)
         word = re.sub(r"^-", '', word)
@@ -30,8 +30,24 @@ def stemmer(sentence_list):
         word = re.sub(r"^\d+\/\d", '', word)
         word = re.sub(r"^\d+", '', word)
         if len(word) is not 0:
-            print(lemmatiser.lemmatize(word, pos='v'))
+            yield lemmatiser.lemmatize(word, pos='v').rstrip().split()
 
+
+class MyCorpus():
+
+    def __init__(self, dictionary, sentence_list):
+        self.dic = dictionary
+        self.sentence = sentence_list
+
+    def __iter__(self):
+        for word in self.sentence:
+            yield self.dic.doc2bow(word.split())
+
+"""
+def create_corpus(dictionary, sentence_list):
+    for word in sentence_list:
+        yield dictionary.doc2bow(word.split())
+"""
 
 if __name__ == '__main__':
     with open('data/sentiment.txt', encoding='utf-8') as f:
@@ -43,4 +59,16 @@ if __name__ == '__main__':
                           .strip().split()
 
     sentence_list = extract_sentence(content_list)
-    stemmer(sentence_list)
+    stem = stemmer(sentence_list)
+    """ 辞書オブジェクトの作成 """
+    dictionary = corpora.Dictionary(stem)  # 17330 size
+    print(dictionary)
+    """ 辞書オブジェクトの語彙の削減 """
+    """
+    # dictionary.filter_extremes(no_below=100, no_above=0.2)  # 100回以上出現してかつ，出現頻度が2割を超えない単語の辞書 138 size
+    corpus = MyCorpus(dictionary, sentence_list)
+    # corpus = create_corpus(dictionary, sentence_list)
+    # corpus = next(iter(corpus))
+    lda = models.LdaModel(corpus, num_topics=60, id2word=dictionary)
+    print(lda.show_topics(num_topics=5))
+    """
