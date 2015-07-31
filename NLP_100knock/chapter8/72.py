0229a2f5b376267f12d42589149b7660e5be828a
Nirperm <nirperm@gmail.com>
2015-07-31 11:42:50 +0900

    finish? section 72

--- NLP_100knock/chapter8/72.py
+++ NLP_100knock/chapter8/72.py
@@ -5,12 +5,14 @@
 各単語をステミング処理したものが最低限のベースラインとなるであろう．
 """
 
+import numpy as np
 import gensim
+from collections import namedtuple
 from constant import STOPWORDS
 from nltk.stem import WordNetLemmatizer
 
 
-if __name__ == '__main__':
+def load_sentiment():
     with open('data/sentiment.txt', encoding='utf-8') as f:
         lines = f.readlines()
 
@@ -18,24 +20,27 @@ if __name__ == '__main__':
     stems = [[lemmatiser.lemmatize(word, pos='v') for word in line.split() if word not in STOPWORDS] for line in lines]  # 10661 size
 
     label_list = [1 if line[:2] == '+1' else 0 for line in lines]
-    # TODO: とりあえず行分のラベルはできたが、単語数には合っていない
+    target = np.asarray(label_list)
 
-    """ 辞書オブジェクトの作成 """
     dictionary = gensim.corpora.Dictionary(stems)
+
     dictionary.save('/tmp/deerwester.dict')
 
     """ 辞書オブジェクトの語彙で低頻度と高頻度のワードは除く """
     dictionary.filter_extremes(no_below=3, no_above=0.6)
 
     corpus = [dictionary.doc2bow(stem) for stem in stems]
+
     gensim.corpora.MmCorpus.serialize('/tmp/deerwester.mm', corpus)
-    dictionary = gensim.corpora.Dictionary.load('/tmp/deerwester.dict')
-    corpus = gensim.corpora.MmCorpus('/tmp/deerwester.mm')
 
-    tfidf = gensim.models.TfidfModel(corpus)
-    corpus_tfidf = tfidf[corpus]
-    lsi = gensim.models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
+    # tfidf = gensim.models.TfidfModel(corpus) FIXME: is this necessary?
+    numpy_matrix = gensim.matutils.corpus2dense(corpus, num_terms=len(corpus))
 
-    corpus_lsi = lsi[corpus_tfidf]
-    for doc in tfidf[corpus]:
-        print(doc)
+    Sentiment = namedtuple("Sentiment", "data target")
+    sentiment = Sentiment(numpy_matrix, label_list)
+    return sentiment
+
+
+if __name__ == '__main__':
+    sentiment = load_sentiment()
+    print(sentiment)
