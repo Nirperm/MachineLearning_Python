5baab9cf2051ff87775212441256db6729b13bf8
nirperm <nirperm@gmail.com>
2015-08-02 23:57:10 +0000

    tweak

--- NLP_100knock/chapter8/73.py
+++ NLP_100knock/chapter8/73.py
@@ -4,23 +4,22 @@
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from section_72 import load_sentiment
 from sklearn import linear_model
 
 if __name__ == '__main__':
     sentiment = load_sentiment()
     X = sentiment.data
-    print(X[:,0])
     Y = sentiment.target
     h = .02  # step size in the mesh
-    logreg = linear_model.LogisticRegression(penalty="l1",C=1e5)
+    logreg = linear_model.LogisticRegression(penalty="l1", C=1e5)
     logreg.fit(X, Y)
 
-    # print(logreg)
-
+    """
+    print(logreg.coef_)
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
     y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
-    # Z =logreg.predict(np.c_[xx.ravel(), yy.ravel()])  10000, 2
+    Z =logreg.predict(np.c_[xx.ravel(), yy.ravel()])  10000, 2
+    """
