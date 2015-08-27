d6651a102c6a120cfbf7e16d1235ce78d2fdb16c
nirperm <nirperm@gmail.com>
2015-08-27 06:54:32 +0000

    implementation(ongoing)

---
+++ NLP_100knock/chapter8/75.py
@@ -0,0 +1,52 @@
+"""
+75. 素性の重み
+73で学習したロジスティック回帰モデルの中で，
+重みの高い素性トップ10と，重みの低い素性トップ10を確認せよ．
+"""
+
+import gensim
+import os
+from section_72 import load_txt
+from section_72 import stem
+from section_72 import create_feature
+from section_73 import modelize
+
+
+def create_dict(stems):
+    dictionary = gensim.corpora.Dictionary(stems)
+    dictionary.save('/tmp/chapter8.dict')
+    return dictionary
+
+
+def create_corpus(dictionary, stems):
+    corpus = [dictionary.doc2bow(stem) for stem in stems]
+    gensim.corpora.MmCorpus.serialize('/tmp/chapter8.mm', corpus)
+
+
+def create_scale(corpus, feature):
+    corpus = [[' '.join(map(str, (list(e)))).replace(' ', ':') for e in ele] for ele in corpus]
+
+    f = open('./data/75.scale', 'w')
+    n = min(len(corpus), len(feature.target))
+    for i in range(n):
+        f.write(str(feature.target[i]) + ' ' + ' '.join([e for e in corpus[i]]) + '\n')
+    f.close()
+
+
+if __name__ == '__main__':
+    lines = load_txt()
+    stems = stem(lines)
+    feature = create_feature(lines, stems)
+    logreg = modelize(feature)
+
+    if 'chapter8.dict' and 'chapter8.mm' not in os.listdir('/tmp/'):
+        dictionary = create_dict(stems)
+        create_corpus(dictionary, stems)
+
+    dictionary = gensim.corpora.Dictionary.load('/tmp/chapter8.dict')
+    corpus = gensim.corpora.MmCorpus('/tmp/chapter8.mm')
+
+    if '75.scale.model' not in os.listdir('./data/'):
+        create_scale(corpus, feature)
+
+    # TODO: use libsvm? & check feature of high and low weight
