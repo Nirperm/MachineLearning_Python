8ea62862659af1c5f076851877bc13fdf9038e84
nirperm <nirperm@gmail.com>
2015-09-02 06:33:28 +0000

    finish 53

--- NLP_100knock/chapter6/53.py
+++ NLP_100knock/chapter6/53.py
@@ -4,25 +4,12 @@ Stanford Core NLPを用い，入力テキストの解析結果をXML形式で得
 また，このXMLファイルを読み込み，入力テキストを1行1単語の形式で出力せよ．
 """
 
-# import corenlp
-from corenlp import batch_parse
-from section_50 import divide_content
-import xmltodict
-
+import xml.etree.ElementTree as ET
 
 if __name__ == '__main__':
-    corenlp_dir = '/usr/local/lib/stanford-corenlp-full-2013-06-20/'
-    raw_text_directory = 'data/'
-    parsed = batch_parse(raw_text_directory, corenlp_dir, raw_output=True)
-    print(parsed)
-    # result = xmltodict.parse(parsed)
-    # print(result)
-    """
-    parser = corenlp.StanfordCoreNLP(corenlp_path=corenlp_dir)
-    sentence_list = divide_content()
-    for word in sentence_list:
-        print(parser.parse(word))
-    # contents = ''.join(sentence_list)
-    # result_json = json.loads(parser.parse(contents))
-    # print(result_json)
-    """
+    tree = ET.parse('data/nlp.xml')
+    root = tree.getroot()
+    words = root.findall('./document/sentences/sentence/tokens/token/word')
+
+    for word in words:
+        print(word.text)
