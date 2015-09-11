8f099de15c76344520c9eb59535b9d1216c01997
Nirperm <nirperm@gmail.com>
2015-09-11 12:03:45 +0900

    tweak 41, 42

--- NLP_100knock/chapter5/section_40.py
+++ NLP_100knock/chapter5/section_40.py
@@ -2,68 +2,40 @@
 40. 係り受け解析結果の読み込み（形態素）
 形態素を表すクラスMorphを実装せよ．
 このクラスは表層形（surface），基本形（base），品詞（pos），品詞細分類1（pos1）をメンバ変数に持つこととする．
-さらに，CaboChaの解析結果（neko.txt.cabocha）を読み込み，各文をMorphオブジェクトのリストとして表現し，3文目の形態素列を表示せよ．
+さらに，CaboChaの解析結果（neko.txt.cabocha）を読み込み，
+各文をMorphオブジェクトのリストとして表現し，3文目の形態素列を表示せよ．
 """
 
 
-import CaboCha
-
-
-def load_txt(filepath):
-    with open(filepath, 'r') as f:
-        txt = f.read()
-    return txt
-
-
-def analyze(txt):
-    """ Chabocha analyze """
-    c = CaboCha.Parser('-f1')
-    tree = c.parse(txt)
-    return tree.toString(CaboCha.FORMAT_LATTICE)
-
-
-def spearate_element(result):
-    cabocha_list = []
-
-    surface_list = []
-    base_list = []
-    pos_list = []
-    pos1_list = []
-    morph_list = []
-
-    for line in result.split('\n'):
-        ary = line.replace('\t', ',').split(',')
-        morph_list.append(ary)
-        cabocha_list.append(morph_list)
-        if len(ary) is 1:
-            pass
-        else:
-            surface_list.append(ary[0])
-            base_list.append(ary[-3])
-            pos_list.append(ary[1])
-            pos1_list.append(ary[2])
-
-            cabocha_list.append(surface_list)
-            cabocha_list.append(base_list)
-            cabocha_list.append(pos_list)
-            cabocha_list.append(pos1_list)
-
-    return cabocha_list
-
-
 class Morph:
+    def __init__(self, mecab_result):
+        result = mecab_result.replace('\t', ',').split(',')
+        self.surface = result[0]
+        self.base = result[7]
+        self.pos = result[1]
+        self.pos1 = result[2]
 
-    def __init__(self, morphs, surface, base, pos, pos1):
-        self.morphs = morphs
-        self.surface = surface
-        self.base = base
-        self.pos = pos
-        self.pos1 = pos1
+    def __str__(self):
+        result = 'surface:%s\t base:%s\t pos:%s\tpos1:%s\t' % (self.surface, self.base, self.pos, self.pos1)
+        return result
+
+
+def make_morphs():
+    sentence_morph_list = []
+    text_morph_list = []
+
+    for line in open('./data/neko.txt.cabocha', 'r'):
+        if line[0] == '*':
+            continue
+        elif line[0:3] == 'EOS':
+            text_morph_list.append(sentence_morph_list)
+            sentence_morph_list = []
+        else:
+            sentence_morph_list.append(Morph(line))
+    return text_morph_list
 
 
 if __name__ == '__main__':
-    txt = load_txt('./data/neko.txt')
-    result = analyze(txt)
-    cabocha_list = spearate_element(result)
-    morph = Morph(cabocha_list[0], cabocha_list[1], cabocha_list[2], cabocha_list[3], cabocha_list[4])
-    print(morph.morphs[3])
+    text_morph_list = make_morphs()
+    for morph in text_morph_list[2]:
+        print(morph)
--- NLP_100knock/chapter5/section_41.py
+++ NLP_100knock/chapter5/section_41.py
@@ -10,43 +10,116 @@
 第5章の残りの問題では，ここで作ったプログラムを活用せよ．
 """
 
-from section_40 import load_txt
-from section_40 import analyze
-from section_40 import spearate_element
-
-
-def read_cabocha(result):
-    dst_list = []
-    srcs_list = []
-    teos_list = []
-
-    for line in result.split('\n'):
-        ary = line.replace('\t', ',').split(',')
-        if len(ary) is 1:
-            teos_ary = ''.join(ary).split(' ')
-            if len(teos_ary) > 1:
-                dst_list.append(teos_ary[1])
-                srcs_list.append(teos_ary[2])
-
-                teos_list.append(dst_list)
-                teos_list.append(srcs_list)
-
-    return teos_list
+from section_40 import Morph
+from collections import defaultdict
 
 
 class Chunk:
+    symbol_list = ['「', '」', '（', '）', '、', '。']
 
-    def __init__(self, morphs, dst, srcs):
-        self.morphs = morphs
+    def __init__(self,  dst, num):
         self.dst = dst
-        self.srcs = srcs
+        self.srcs = list()
+        self.morphs = []
+        self.num = num
+
+    def append_morph(self, mecab_result):
+        self.morphs.append(Morph(mecab_result))
+
+    def __str__(self):
+        string = ''
+        for morph in self.morphs:
+            string += morph.surface
+        return string + '\t' + self.dst + '\t' + ','.join(self.srcs)
+
+    def get_phrase(self):
+        phrase = ''
+        for morph in self.morphs:
+            if morph.surface in self.symbol_list:
+                continue
+            phrase += morph.surface
+        return phrase
+
+    def check_phrase_pos(self, pos):
+        for morph in self.morphs:
+            if morph.pos == pos:
+                return True
+        return False
+
+    def get_pos_word(self, pos):
+        phrase = ''
+        for morph in self.morphs:
+            if morph.pos == pos:
+                if pos == '助詞':
+                    phrase += morph.surface + ' '
+                elif pos == '動詞':
+                    phrase += morph.base
+                    break
+        return phrase
+
+    def check_phrase_pos1(self, pos1):
+        for morph in self.morphs:
+            if morph.pos1 == pos1:
+                return True
+        return False
+
+    def check_phrase_word(self, word):
+        for morph in self.morphs:
+            if morph.surface == word:
+                return True
+        return False
+
+    def phrase_replace(self, pos, re_word):
+        phrase = ''
+        for morph in self.morphs:
+            if morph.surface in self.symbol_list:
+                continue
+            if morph.pos == pos:
+                if phrase not in re_word:
+                    phrase += re_word
+            else:
+                phrase += morph.surface
+        return phrase
+
+    def root_word(self, sentence_chunk_list, dst):
+        word_dst = dst
+        while word_dst != '-1':
+            word_dst = sentence_chunk_list[word_dst].dst
+        return sentence_chunk_list[word_dst].get_phrase()
+
+
+def make_text_chunk():
+    text_chunk_list = []
+    sentence_chunk_list = []
+    srcs_dict = defaultdict(list)
+    count = 0
+    for line in open('./data/neko.txt.cabocha', 'r'):
+        if line[:3] == 'EOS':
+            count = 0
+            for num in range(len(sentence_chunk_list)):
+                sentence_chunk_list[num].srcs = srcs_dict[str(num)]
+            text_chunk_list.append(sentence_chunk_list)
+
+            sentence_chunk_list = []
+            srcs_dict = defaultdict(list)
+
+        elif line[:2] == '* ':
+            chunk_info = line.strip().split(' ')
+            chunk_info[2] = chunk_info[2].rstrip('D')
+            phrase_chunk = Chunk(chunk_info[2], count)
+            srcs_dict[chunk_info[2]].append(chunk_info[1])
+            sentence_chunk_list.append(phrase_chunk)
+            count += 1
+        else:
+            phrase_chunk.morphs.append(Morph(line))
+    return text_chunk_list
 
 
 if __name__ == '__main__':
-    txt = load_txt('./data/neko.txt')
-    result = analyze(txt)
-    cabocha_list = spearate_element(result)
-
-    teos_list = read_cabocha(result)
-    chunk = Chunk(cabocha_list[0], teos_list[0], teos_list[1])
-    print(chunk.morphs[8], chunk.dst[8])
+    text_chunk_list = make_text_chunk()
+    count = 0
+    for sentence_chunk_list in text_chunk_list:
+        count += 1
+        for chunk in sentence_chunk_list:
+            if count == 8:
+                print(chunk)
