2c1bc412b1167310c74d07760d83081090f7bfa0
nirperm <nirperm@gmail.com>
2015-09-14 07:12:14 +0000

    tweak 90

--- NLP_100knock/chapter10/90.py
+++ NLP_100knock/chapter10/90.py
@@ -4,29 +4,35 @@
 さらに，学習した単語ベクトルの形式を変換し，86-89のプログラムを動かせ．
 """
 
-# FIXME: Too heavy
 
 from gensim.models import word2vec
 
 
-def section_86():
-    print 'section_86'
-    print(model['United_States'])
+def section_86(model):
+    print('section_86')
+    print(model['United_States'], '\n')
 
-def section_87():
-    print 'section_87'
-    print(model.n_similarity(['United_States'], ['U.S']))
 
-def section_88():
-    print 'section_88'
-    for w,s in  model.most_similar(['England']):
+def section_87(model):
+    print('section_87')
+    print(model.n_similarity(['United_States'], ['U.S']), '\n')
+
+
+def section_88(model):
+    print('section_88')
+    for w, s in model.most_similar(['England']):
         print(w, s)
 
-def section_89():
-    print 'section_89'
-    print model['Spain'] + model['Athens'] - model['Madrid']
-    for w,s in  model.most_similar(['Spain', 'Athens'], ['Madrid']):
+
+def section_89(model):
+    print('section_89')
+    print(model['Spain'] + model['Athens'] - model['Madrid'], '\n')
+    for w, s in model.most_similar(['Spain', 'Athens'], ['Madrid']):
         print(w, s)
 
 if __name__ == '__main__':
     model = word2vec.Word2Vec.load_word2vec_format('data/fb_post.bin', binary=True)
+    section_86(model)
+    section_87(model)
+    section_88(model)
+    section_89(model)
